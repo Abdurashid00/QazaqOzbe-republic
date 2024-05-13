85af1d6e65ef6f8d75bdef9b20494f886905ba7a
unknown <khaidarovabdurashid@gmail.com>
2024-05-13 23:02:15 +0300

    main part 1 finished

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,10 +2,13 @@ window.addEventListener('scroll', function() {
     var header = document.querySelector('.header');
     var scrollPosition = window.scrollY;
 
-    if (scrollPosition > 1) {
+    if (scrollPosition > 20) {
         header.classList.add('black-bg');
     } else {
         header.classList.remove('black-bg');
     }
 });
 //when user scroll header, the function works and bg-color will change
+
+
+
